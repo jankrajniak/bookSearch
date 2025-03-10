afc2ac6c5c5bbf6bfb1bae8cf9c2a71dea9dc91a
jankrajniak <jan.krajniak@gmail.com>
2025-03-10 15:39:31 -0400

    created queries and mutations on client side

---
+++ Develop/client/src/utils/mutations.ts
@@ -0,0 +1,68 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($input: LoginInput!) {
+   login(input: $input) {
+    token
+    user {
+      _id
+      username
+      email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($input: AddUserInput!) {
+    addUser(input: $input) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+  
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!) {
+      saveBook(input: $input) {
+        _id
+        username
+        email
+        password
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+          }
+        bookCount
+      }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($input: BookInput!) {
+    removeBook(input: $input) {
+      _id
+      username
+      email
+      password
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+        }
+    bookCount
+    }
+  }
+`;
+
