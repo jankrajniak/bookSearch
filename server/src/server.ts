0e9ae2ea65dac35227f705f19ba9c7ae2ccb5cd4
jankrajniak <jan.krajniak@gmail.com>
2025-03-13 20:14:59 -0400

    corrected file path referecens in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
@@ -11,6 +12,9 @@ const server = new ApolloServer({
   typeDefs,
   resolvers
 })
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const startApolloServer = async () => {
   await server.start();
@@ -28,10 +32,10 @@ const startApolloServer = async () => {
   }));
 
   if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     })
   }
 
